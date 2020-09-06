092e0fe6922eef4d99a249d2d1948c145e097248
Morsalin-pip <69422979+Morsalin-pip@users.noreply.github.com>
2020-09-06 04:35:45 -1100

    commit

---
+++ log-in.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>log-in</title>
+    <link rel="stylesheet" href="/css/about.css">
+    <link rel="stylesheet" href="/css/all.min.css">
+    <link rel="stylesheet" href="/css/fontawesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@600&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif:wght@700&family=Roboto+Mono:wght@600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif:wght@700&family=Roboto+Mono:wght@600&family=Russo+One&display=swap" rel="stylesheet"> 
+
+    <link rel="stylesheet" href="./css/log-in.css">
+
+
+</head>
+<body>
+    <header id="header-area">
+        <div class="top-header">
+          <div class="logo">
+            <h1 id="logo-h1"><i class="fab fa-affiliatetheme"></i>CoderFox</h1>
+                <ul class="links">
+                    <li><a href="#"><i class="fab fa-youtube fa-2x"></i></a></li>
+                    <li><a href="#"><i class="fab fa-twitter  fa-2x"></i></a></li>
+                    <li><a href="#"><i class="fab fa-linkedin-in fa-2x"></i></a></li>
+                    <li><a href="#"><i class="fab fa-facebook fa-2x"></i></a></li>
+                </ul>
+          </div>
+          <nav>
+            <ul class="ul-container">
+              <li>
+                <a href="index.php">Home</a>
+              </li>
+              <li>
+                <a href="about.php">About us</a>
+              </li>
+              <li>
+                <a href="services.php">Services</a>
+              </li>
+              <li>
+                <a href="sign-up.php">Sign-up</a>
+              </li>
+              <li>
+                <a href="log-in.php">log-in</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+</body>
+</html>
